c0ac0ab3264bcdc3a0d65b11f2c98cd287d7ad16
gomesluiz <gomesluiz@gmail.com>
2022-05-03 17:24:27 -0300

    Revert "include data-science scripts"
    This reverts commit 8f23c1193ca5733d0bf3896cb28b14d585fd919c.

--- algorithms/hackerrank/find_runner_up.py
+++
@@ -1,8 +0,0 @@
-def find_runner_up(n, scores):
-    """Find the runner up"""
-    return sorted(list(set(scores)), reverse=True)[1]
-
-if __name__ == '__main__':
-    # Testing.
-    assert find_runner_up(5, [2, 3, 6, 6, 5]) == 5, "Should be 5"
-    print("Everything passed!")
